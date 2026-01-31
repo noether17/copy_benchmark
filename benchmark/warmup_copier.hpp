3467613e4bd175d38f668653eb8ddc938d9cab61
Jonathan White <jawh@umich.edu>
2026-01-31 12:29:08 -0700

    Removed redundant include directory.

---
+++ benchmark/warmup_copier.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <cstdint>
+
+inline void warmup_copier(std::uint8_t*, std::uint8_t const*, std::size_t) {}
--- include/warmup_copier.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "instantiate_copiers.hpp"
-
-template <typename T>
-void warmup_copier(T*, T const*, std::size_t) {}
-
-INSTANTIATE_COPIERS(warmup_copier);
